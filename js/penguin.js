e3cf07dcf78fd542c6a9a695342a0c5566ac527e
Rony163 <minhajulislamrony43@gmail.com>
2021-08-17 20:11:54 +0600

    first commit

---
+++ js/penguin.js
@@ -0,0 +1,4 @@
+document.getElementById('buy.btn').addEventListener('click', function () {
+    const penguin = document.getElementById('penguin-text');
+    penguin.innerText = 'I AM PENGUIN';
+});
